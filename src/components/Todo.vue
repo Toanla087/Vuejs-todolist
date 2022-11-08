82d86facf020de9a75277090ecf569f79e746a58
Toanla087 <minhtoan0807@gmail.com>
2022-11-09 01:57:17 +0700

    Fix UI, add search todo

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -3,37 +3,57 @@
         <div class="task">
             <div class="newtask">
                 <h1>New Task</h1>
-                <input type="text" v-model="newTask.name" class="todo" placeholder="Add New Task...">
-                <label>Description</label>
-                <textarea v-model="newTask.des" ></textarea>
-                <input type="date" v-model="newTask.date" >
-                <select name="Priority" id="Priority" v-model="newTask.pri">
-                    <option value="Normal" selected>Normal</option>
-                    <option value="Low">Low</option>
-                    <option value="High">High</option>
-                </select>
-                <button @click="addTask()" class="btn add-btn">Add</button>
+                <input type="text" v-model="newTask.name" class="todo-name" placeholder="Add New Task...">
+                <label class="label-des">Description</label>
+                <textarea v-model="newTask.des" class="todo-des"></textarea>
+                <div class="date-pri">
+                    <div class="due-date">
+                        <label class="label-date">Due date</label>
+                        <input type="date" v-model="newTask.date" class="todo-date">
+                    </div>
+                    <div class="priority">
+                        <label class="label-priority">Priority</label>
+                        <select name="Priority" id="Priority" v-model="newTask.pri" class="todo-priority">
+                            <option value="Normal" selected>Normal</option>
+                            <option value="Low">Low</option>
+                            <option value="High">High</option>
+                        </select>
+                    </div>
+                </div>
+                <button @click="addTask()" class="btn">Add</button>
             </div>
         </div>
-        <div class="todo-task">
-            <div v-for="(task,index) in tasks" :key="index" class="task-list">
-                <div class="task-name">
-                    <input type="checkbox" v-model="task.done" class="check-list">
-                    <input type="text" v-model="task.name" :disabled="inputDisabled" :class="{done: task.done}" class="todo todo-item">
-                    <button @click="inputDisabled = !inputDisabled" class="btn edit-btn">Edit</button>
-                    <button @click="removeTask(index)" class="btn remove-btn">Remove</button>
-                </div>
-                <div class="task-detail">
-                    <input type="text" v-model="task.name" class="todo" :disabled="inputDisabled">
-                    <label>Description</label>
-                    <textarea v-model="task.des" ></textarea>
-                    <input type="date" v-model="task.date" >
-                    <select name="Priority" id="Priority" v-model="task.pri" >
-                        <option value="Normal">Normal</option>
-                        <option value="Low">Low</option>
-                        <option value="High">High</option>
-                    </select>
-                    <!-- <button @click="addTask()" class="btn add-btn">Add</button> -->
+        <div class="todo">
+            <div class="todo-task">
+                <h1>To Do List</h1>
+                <input type="text" v-model="search" placeholder="Search..." class="todo-search">
+                <div v-for="(task,index) in filtertasks" :key="index" class="task-list">
+                    <div class="task-name">
+                        <input type="checkbox" v-model="task.done" class="check-list">
+                        <input type="text" v-model="task.name" :disabled="inputDisabled" :class="{done: task.done}" class="todo-name todo-item">
+                        <button @click="inputDisabled = !inputDisabled" class=" edit-btn">Detail</button>
+                        <button @click="removeTask(index)" class=" remove-btn">Remove</button>
+                    </div>
+                    <div class="task-detail">
+                        <input type="text" v-model="task.name" class="todo-name" :disabled="inputDisabled">
+                        <label class="label-des">Description</label>
+                        <textarea v-model="task.des" class="todo-des"></textarea>
+                        <div class="date-pri">
+                            <div class="due-date">
+                                <label class="label-date">Due date</label>
+                                <input type="date" v-model="task.date" class="todo-date">
+                            </div>
+                            <div class="priority">
+                                <label class="label-priority">Priority</label>
+                                <select name="Priority" id="Priority" v-model="task.pri" class="todo-priority">
+                                    <option value="Normal">Normal</option>
+                                    <option value="Low">Low</option>
+                                    <option value="High">High</option>
+                                </select>
+                            </div>
+                        </div>
+                        <button @click="addTask()" class="btn">Update</button>
+                    </div>
                 </div>
             </div>
         </div>
@@ -51,6 +71,7 @@
             date: new Date().toISOString().slice(0,10),
             pri: 'Normal',
           },
+          search:'',
           tasks: [
               {name: 'toan la', des: 'hêheheh', date: '15/12/2022', pri: 'Normal', done: false},
               {name: 'olala', des: 'hêheheh', date: '15/12/2022', pri: 'Normal', done: false},
@@ -58,6 +79,13 @@
           ],
           inputDisabled: true,
       }
+    },
+    computed: {
+        filtertasks: function() {
+            return this.tasks.filter((task) => {
+                return task.name.match(this.search);
+            })
+        }
     },
     methods: {
         addTask: function() {
@@ -88,15 +116,24 @@
       background-color: #fafafa;
       display: flex;
       justify-content: center;
+      box-sizing: border-box;
     }
-    .task, .todo-task {
+    .task, .todo {
       display: inline-block;
+      border: 1px solid #000;
     }
-  .newtask {
+
+    .newtask {
+        margin: 30px;
+    }
+    .todo-task {
+        margin: 65px;
+    }
+  .newtask, .task-detail {
     display: flex;
     flex-direction: column;
     justify-content: center;
-    box-sizing: border-box;
+    width: 600px;
   }
 
   .task-list {
@@ -106,8 +143,8 @@
     box-sizing: border-box;
   }
   .task-name {
-    display: block;
-
+    display: flex;
+    align-items: center;
   }
   .done {
       text-decoration: line-through;
@@ -115,23 +152,92 @@
   h1 {
       font-size: 50px;
   }
-  .todo {
-      width: 52%;
+  .todo-name {
+      width: calc(100%-8px);
       height: 40px;
-      margin: 10px;
-      font-size: 40px;
+      font-size: 20px;
   }
-  .btn {
-      width: 10%;
-      height: 100%;
-      margin: 10px;
+
+  .label-des {
+    text-align: left;
+    font-weight: 700;
+    margin: 30px 0 10px 0;
   }
-  .btn:hover {
-      cursor: pointer;
-  } 
-  .add-btn {
-      font-size: 36px;
+
+  .todo-des {
+    width: calc(100%-6px);
+    height: 200px;
+    border: 1px solid #000;
   }
+  
+  .date-pri {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .due-date, .priority {
+    display: flex;
+    flex-direction: column;
+    width: 275px;
+  }
+
+  .label-date {
+    text-align: left;
+    font-weight: 700;
+    margin: 30px 0 10px 0;
+  }
+
+  .todo-date {
+    width: 100%;
+    height: 38px;
+    border: 1px solid #000;
+    }
+
+    .label-priority {
+        text-align: left;
+        font-weight: 700;
+        margin: 30px 0 10px 0;
+    }
+
+    .todo-priority {
+        width: 100%;
+        height: 40px;
+        border: 1px solid #000;
+    }
+    .btn {
+        display: block;
+        font-size: 30px;
+        padding: 10px;
+        margin-top: 50px;
+        width: 100%;
+        border-radius: 4px;
+        border: none;
+        color: #fff;
+        background-color: #5cb85c;
+    }
+ 
+    .btn:hover {
+        cursor: pointer;
+    } 
+
+    .todo-search {
+        width: 646px;
+        height: 40px;
+        font-size: 20px;
+    }
+    .task-name {
+        margin: 20px 20px 0 20px;
+        border: 1px solid #000;
+        width: 650px;
+        height: 100px;
+    }
+
+    .task-detail {
+        border: 1px solid #000;
+        border-top: none;
+        margin: 0 0 20px 20px;
+        padding: 25px;
+    }
   .check-list {
       width: 20px;
       height: 20px;
